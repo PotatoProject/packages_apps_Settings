d32f278e28296de42f130c69e6efc31c270113f5
Harry Youd <harry@harryyoud.co.uk>
2017-09-27 19:48:42 +0100

    Settings: Add Updater
    Adapted to POSP by: 4PERTURE <slicey2001@gmail.com>
    
    Adding to both "System" (top category), and "About phone"
    (subcategory of "System"), similar to that of the GMS System Update.
    
    AOSiP Edit - Removed from "About Phone"
    AOSiP Edit - Fixed for Pie
    
    Change-Id: Icca0f96720d0851738490ad543b54ec82cb19fa2
    Signed-off-by: Brett Montague <bmontagu@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    Signed-off-by: xlxfoxxlx <joshfox87@gmail.com>
    Signed-off-by: argraur <argraur@gmail.com>

--- src/com/android/settings/deviceinfo/DeviceInfoSettings.java
+++ src/com/android/settings/deviceinfo/DeviceInfoSettings.java
@@ -32,6 +32,7 @@ import com.android.settings.deviceinfo.firmwareversion.FirmwareVersionPreference
 import com.android.settings.deviceinfo.potato.PotatoVersionPreferenceController;
 import com.android.settings.deviceinfo.imei.ImeiInfoPreferenceController;
 import com.android.settings.deviceinfo.simstatus.SimStatusPreferenceController;
+import com.android.settings.deviceinfo.UpdatePreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -139,6 +140,7 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
         controllers.add(new ManualPreferenceController(context));
         controllers.add(new FeedbackPreferenceController(fragment, context));
         controllers.add(new FccEquipmentIdPreferenceController(context));
+	controllers.add(new UpdatePreferenceController(context));
         controllers.add(
                 new BuildNumberPreferenceController(context, activity, fragment, lifecycle));
         return controllers;
