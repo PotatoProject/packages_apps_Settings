448b618fe01d5f03ed7f85465f79b45960ec632e
Shreyansh Lodha <slodha96@gmail.com>
2018-09-21 09:21:29 +0530

    Add Dialog for users to know POSP Details
    Change-Id: Ie8c3c26a1e9a87e14eac19e896c1023c5647699c
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

---
+++ src/com/android/settings/deviceinfo/potato/PotatoVersionDialogFragment.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2018 The Potato Open Sauce Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.potato;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.core.instrumentation.InstrumentedDialogFragment;
+
+public class PotatoVersionDialogFragment extends InstrumentedDialogFragment {
+
+    private static final String TAG = "potatoVersionDialog";
+
+    private View mRootView;
+
+    public static void show(Fragment host) {
+        final FragmentManager manager = host.getChildFragmentManager();
+        if (manager.findFragmentByTag(TAG) == null) {
+            final PotatoVersionDialogFragment dialog = new PotatoVersionDialogFragment();
+            dialog.show(manager, TAG);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.POTATO_WEDGES;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.potato_title)
+                .setPositiveButton(android.R.string.ok, null /* listener */);
+
+        mRootView = LayoutInflater.from(getActivity()).inflate(
+                R.layout.dialog_potato_version, null /* parent */);
+
+        initializeControllers();
+
+        return builder.setView(mRootView).create();
+    }
+
+    public void setText(int viewId, CharSequence text) {
+        final TextView view = mRootView.findViewById(viewId);
+        if (view != null) {
+            view.setText(text);
+        }
+    }
+
+    public void removeSettingFromScreen(int viewId) {
+        final View view = mRootView.findViewById(viewId);
+        if (view != null) {
+            view.setVisibility(View.GONE);
+        }
+    }
+
+    public void registerClickListener(int viewId, View.OnClickListener listener) {
+        final View view = mRootView.findViewById(viewId);
+        if (view != null) {
+            view.setOnClickListener(listener);
+        }
+    }
+
+    private void initializeControllers() {
+        // Initialize all properties from here.
+        // example : new FirmwareVersionDialogController(this).initialize();
+        new PotatoVersionNumberController(this).initialize();
+        new PotatoVersionNameController(this).initialize();
+        new PotatoVersionStatusController(this).initialize();
+        new PotatoVersionMaintainerController(this).initialize();
+        new PotatoVersionHostController(this).initialize();
+        new PotatoVersionBuildTypeController(this).initialize();
+        new PotatoVersionBuildDateController(this).initialize();
+    }
+}
