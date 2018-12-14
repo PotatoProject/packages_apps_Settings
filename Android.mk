0efa04c50c9dfff69678d4de8bfad0dbe3b17a96
Kshitij Gupta <kshitijgm@gmail.com>
2018-12-14 14:49:23 +0530

    Settings: Suppress unused source files warning
    Change-Id: I8ceaf6062e13bf6f0528a0eeed0a0b8e0e88d769
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- Android.mk
+++ Android.mk
@@ -18,8 +18,9 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
 LOCAL_USE_AAPT2 := true
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src) \
-LOCAL_SRC_FILES += $(call all-java-files-under, ../Fries/src)
+LOCAL_SRC_FILES := \
+    $(call all-java-files-under, src) \
+    $(call all-java-files-under, ../Fries/src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     $(ANDROID_SUPPORT_DESIGN_TARGETS) \
