fc5d91c208096cc0394ec986c420c0b9a328bdd1
sarveshrulz <sarveshkardekar@gmail.com>
2020-06-05 18:24:17 +0530

    Settings: Move refresh rate settings to display
    Change-Id: I90e08bbe211db233cd313e32ff7b563a5d3d39fb

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -33,6 +33,7 @@ import com.android.settings.display.TapToWakePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.TimeoutPreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
+import com.android.settings.display.RefreshRatePreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -92,6 +93,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
+        controllers.add(new RefreshRatePreferenceController(context));
         return controllers;
     }
 
